f110a929d3bf5c2302588f37bb618bd57482790f
Ike Kurghinyan <mail@dempfi.com>
2026-01-16 05:58:10 +0100

    Add 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config'
 import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: 'https://ayu-theme.github.io',
+  site: 'https://ayutheme.com',
   build: {
     assets: '_assets',
     inlineStylesheets: 'always',
   },
   scopedStyleStrategy: 'class',
   integrations: [
+    sitemap(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
